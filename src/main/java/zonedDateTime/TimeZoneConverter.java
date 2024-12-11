595c60c931307391e08ea7d53d65f0141ff8c164
gooinsung <gooinsung96@gmail.com>
2024-12-11 22:13:12 +0900

    convertToFormattedString, TimeZoneConverterTest.java

--- src/main/java/zonedDateTime/TimeZoneConverter.java
+++ src/main/java/zonedDateTime/TimeZoneConverter.java
@@ -6,10 +6,11 @@ import zone.ZoneDateTimeConverter;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class TimeZoneConverter {
 
-    public ZonedDateTime zonedDateTimeConvert(String dateTime, String originFormat, String originTimeZone, String targetTimeZone) {
+    public static ZonedDateTime zonedDateTimeConvert(String dateTime, String originFormat, String originTimeZone, String targetTimeZone) {
         LocalDateTime originLocalDateTime = FormatConverter.getLocalDateTime(dateTime, originFormat);
 
         ZoneId originZoneId = ZoneDateTimeConverter.makeZoneId(originTimeZone);
@@ -18,7 +19,7 @@ public class TimeZoneConverter {
         return zonedDateTimeConvert(originLocalDateTime, originZoneId, targetZoneId);
     }
 
-    public ZonedDateTime zonedDateTimeConvert(String dateTime, String originTimeZone, String targetTimeZone) {
+    public static ZonedDateTime zonedDateTimeConvert(String dateTime, String originTimeZone, String targetTimeZone) {
 
         ZoneId originZoneId = ZoneDateTimeConverter.makeZoneId(originTimeZone);
         ZoneId targetZoneId = ZoneDateTimeConverter.makeZoneId(targetTimeZone);
@@ -26,7 +27,7 @@ public class TimeZoneConverter {
         return zonedDateTimeConvert(dateTime, originZoneId, targetZoneId);
     }
 
-    public ZonedDateTime zonedDateTimeConvert(LocalDateTime localDateTime, String originTimeZone, String targetTimeZone) {
+    public static ZonedDateTime zonedDateTimeConvert(LocalDateTime localDateTime, String originTimeZone, String targetTimeZone) {
 
         ZoneId originZoneId = ZoneDateTimeConverter.makeZoneId(originTimeZone);
         ZoneId targetZoneId = ZoneDateTimeConverter.makeZoneId(targetTimeZone);
@@ -34,20 +35,27 @@ public class TimeZoneConverter {
         return zonedDateTimeConvert(localDateTime, originZoneId, targetZoneId);
     }
 
-    public ZonedDateTime zonedDateTimeConvert(String dateTime, ZoneId originZoneId, ZoneId targetZoneId) {
+    public static ZonedDateTime zonedDateTimeConvert(String dateTime, ZoneId originZoneId, ZoneId targetZoneId) {
 
         LocalDateTime originLocalDateTime = FormatConverter.getLocalDateTime(dateTime);
 
         return zonedDateTimeConvert(ZonedDateTime.of(originLocalDateTime, originZoneId), targetZoneId);
     }
 
-    public ZonedDateTime zonedDateTimeConvert(LocalDateTime localDateTime, ZoneId originZoneId, ZoneId targetZoneId) {
+    public static ZonedDateTime zonedDateTimeConvert(LocalDateTime localDateTime, ZoneId originZoneId, ZoneId targetZoneId) {
         ZonedDateTime originZonedDateTime = ZonedDateTime.of(localDateTime, originZoneId);
         return zonedDateTimeConvert(originZonedDateTime, targetZoneId);
     }
 
-    public ZonedDateTime zonedDateTimeConvert(ZonedDateTime zonedDateTime, ZoneId targetZone) {
-        return zonedDateTime.withZoneSameLocal(targetZone);
+    public static ZonedDateTime zonedDateTimeConvert(ZonedDateTime zonedDateTime, ZoneId targetZone) {
+        return zonedDateTime.withZoneSameInstant(targetZone);
+    }
+
+    public static String convertToFormattedString(String dateTime, String originTimeZone, String targetTimeZone, String targetFormat) {
+        LocalDateTime originLocalDateTime = FormatConverter.getLocalDateTime(dateTime);
+        ZonedDateTime convertedZonedDateTime = zonedDateTimeConvert(originLocalDateTime, originTimeZone, targetTimeZone);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(targetFormat);
+        return formatter.format(convertedZonedDateTime);
     }
 
 }
